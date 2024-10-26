316e5c1a0b55c7d7061b28e69d6f71186caa61e2
vrushgithub <cvrushabh01@gmail.com>
2024-10-26 16:25:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import { Typography, Box, styled } from '@mui/material';
 import './App.css';
 
+import Balance from './Components/Balance';
+import ExpenseCard from './Components/ExpenseCard';
+import Transactions from './Components/Transactions';
+import NewTransaction from './Components/NewTransaction';
+
+const Header = styled(Typography)`
+  margin: 10px 0;
+  color: blue;
+  font-size: 36px;
+  text-transform: uppercase;
+`;
+
+const Component = styled(Box)`
+  background: #FFF;
+  padding: 10px;
+  border-radius: 20px;
+  display: flex;
+  width: 800px;
+  & > div {
+    padding: 10px;
+    width: 50%;
+    height: 70vh;
+  }
+}
+`;
+
 function App() {
+  
+  const [transactions, setTransactions] = useState([
+    { id: 1, text: 'Momos', amount: -200},
+    { id: 2, text: 'Salary', amount: 30000},
+    { id: 3, text: 'Book', amount: -1000},
+    { id: 4, text: 'Bonus', amount: 10000 },
+  ]);
+
+  const deleteTransaction = (id) => {
+    console.log(id);
+    setTransactions(transactions.filter(transaction => transaction.id !== id));
+    console.log(transactions);
+  }
+
+  const addTransaction = (transaction) => {
+    setTransactions(transactions => [transaction, ...transactions]);
+    console.log(transaction);
+    console.log(transactions);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>Expense Tracker</Header>
+      <Component>
+        <Box>
+          <Balance transactions={transactions} />
+          <ExpenseCard transactions={transactions} />
+          <NewTransaction addTransaction={addTransaction}/>
+        </Box>
+        <Box>
+          <Transactions transactions={transactions} deleteTransaction={deleteTransaction}/>
+        </Box>
+      </Component>
     </div>
   );
 }
